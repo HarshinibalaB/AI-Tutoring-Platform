b0dc3de2da562ceae922db25e428ad529f6b9f4b
HarshinibalaB <harshinibharathi2408@gmail.com>
2024-08-15 23:49:57 +0530

    Create knowledge_graph.py

---
+++ knowledge_graph.py
@@ -0,0 +1,16 @@
+class KnowledgeGraph:
+    def __init__(self):
+        self.graph = {}
+
+    def add_concept(self, concept):
+        if concept not in self.graph:
+            self.graph[concept] = []
+
+    def add_relationship(self, parent, child):
+        if parent in self.graph:
+            self.graph[parent].append(child)
+        else:
+            self.graph[parent] = [child]
+
+    def get_related_concepts(self, concept):
+        return self.graph.get(concept, [])
